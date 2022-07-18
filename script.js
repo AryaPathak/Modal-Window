3896a3125f3b01f73b07614356643e963035e5d7
arya <a@gmail.com>
2022-07-18 13:26:40 +0530

    Source Code Pushed

---
+++ script.js
@@ -0,0 +1,29 @@
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnColseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+const open = function() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+const close = function (){
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+for(let i=0;i<btnsOpenModal.length;i++){
+  (btnsOpenModal[i].addEventListener('click', open));
+}
+
+btnColseModal.addEventListener('click', close);
+
+overlay.addEventListener('click', close);
+
+
+document.addEventListener('keydown', function(e){
+  if(e.key=='Escape' && !modal.classList.contains('hidden')){
+    close();
+  }
+});
